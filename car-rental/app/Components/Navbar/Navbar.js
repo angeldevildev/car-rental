d88a335ff6948d949877b3299247eb342f30a55f
mineshopdev <mineshop47@gmail.com>
2023-09-13 21:44:18 +0200

    Updated the Navbar.js file

--- car-rental/app/Components/Navbar/Navbar.js
+++ car-rental/app/Components/Navbar/Navbar.js
@@ -66,7 +66,7 @@ function Navbar() {
             </li>
             <li>
               <a className="about-link" to="/">
-                About
+                About us
               </a>
             </li>
             <li>
