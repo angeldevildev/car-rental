078d21b468563110b31ff52a86d7412c1cffcfea
mineshopdev <mineshop47@gmail.com>
2023-09-16 22:54:02 +0200

    Hero image weight optimization

--- app/Components/Hero/Hero.js
+++ app/Components/Hero/Hero.js
@@ -1,6 +1,6 @@
 'use client'
 
-import BgShape from "./hero-bg.png";
+import BgShape from "./hero-bg.webp";
 import HeroCar from "./main-car.webp";
 import { useEffect, useState } from "react";
 import { IconChevronRight, IconCircleCheck } from "@tabler/icons-react";
