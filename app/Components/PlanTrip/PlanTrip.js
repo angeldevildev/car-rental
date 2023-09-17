ce033cf5f8f63cc38c3b618b9c9791f9c038d8e7
mineshopdev <mineshop47@gmail.com>
2023-09-17 15:34:49 +0200

    Fixing the '' can be escaped error

--- app/Components/PlanTrip/PlanTrip.js
+++ app/Components/PlanTrip/PlanTrip.js
@@ -36,10 +36,14 @@ function PlanTrip() {
 
               <div className="plan-container__boxes__box">
                 <Image src={Drive} alt="icon_img" />
-                <h3>Let's Drive</h3>
+                <h3>{`Let's Drive`}</h3>
                 <p>
-                  Whether you're hitting the open road, we&apos;ve got you covered
+                {
+                `
+                  Whether you're hitting the open road, we've got you covered
                   with our wide range of cars
+                `
+                }
                 </p>
               </div>
             </div>
