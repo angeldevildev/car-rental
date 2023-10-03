0e19c2114cff1ac345430c1f0b8bb6c84a90fdbe
mineshopdev <mineshop47@gmail.com>
2023-10-03 21:06:00 +0200

    Added animation to Plan Trip

--- app/Components/PlanTrip/PlanTrip.js
+++ app/Components/PlanTrip/PlanTrip.js
@@ -1,48 +1,52 @@
+'use client'
+
 import SelectCar from "./icon1.svg";
 import Contact from "./icon2.svg";
 import Drive from "./icon3.svg";
 import Image from "next/image";
 import "./PlanTrip.css";
+import { ScrollReveal } from "reveal-on-scroll-react"
+
 
 function PlanTrip() {
   return (
     <>
-      <section className="plan-section">
+      <section className="plan-section" id="about">
         <div className="container">
           <div className="plan-container">
             <div className="plan-container__title">
-              <h3>Plan your trip now</h3>
-              <h2>Quick &amp; easy car rental</h2>
+              <ScrollReveal.h3>Plan your trip now</ScrollReveal.h3>
+              <ScrollReveal.h2>Quick &amp; easy car rental</ScrollReveal.h2>
             </div>
 
             <div className="plan-container__boxes">
               <div className="plan-container__boxes__box">
                 <Image src={SelectCar} alt="icon_img" loading="lazy" />
-                <h3>Select Car</h3>
-                <p>
+                <ScrollReveal.h3>Select Car</ScrollReveal.h3>
+                <ScrollReveal.p>
                   We offer a big range of vehicles for all your driving needs.
                   We have the perfect car to meet your needs
-                </p>
+                </ScrollReveal.p>
               </div>
 
               <div className="plan-container__boxes__box">
                 <Image src={Contact} alt="icon_img" loading="lazy" />
-                <h3>Contact Operator</h3>
-                <p>
+                <ScrollReveal.h3>Contact Operator</ScrollReveal.h3>
+                <ScrollReveal.p>
                   Our knowledgeable and friendly operators are always ready to
                   help with any questions or concerns
-                </p>
+                </ScrollReveal.p>
               </div>
 
               <div className="plan-container__boxes__box">
                 <Image src={Drive} alt="icon_img" loading="lazy" />
-                <h3>{`Let's Drive`}</h3>
-                <p>
+                <ScrollReveal.h3>{`Let's Drive`}</ScrollReveal.h3>
+                <ScrollReveal.p>
                   {`
                   Whether you're hitting the open road, we've got you covered
                   with our wide range of cars
                 `}
-                </p>
+                </ScrollReveal.p>
               </div>
             </div>
           </div>
